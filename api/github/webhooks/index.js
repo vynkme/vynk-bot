aa7bccc4746d3ff5bb6d0d9b0807c59cf34a4a8a
Maanas Nair <bee.birdiecorporation@gmail.com>
2021-09-19 17:08:46 +0530

    fix typo

--- api/github/webhooks/index.js
+++ api/github/webhooks/index.js
@@ -1,6 +1,6 @@
 const { createNodeMiddleware, createProbot } = require("probot");
 
-const app = require("../../../app");
+const app = require("../../../index");
 
 module.exports = createNodeMiddleware(app, {
   probot: createProbot(),
