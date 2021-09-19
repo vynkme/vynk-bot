0c2f810b5930b84884bb47551ee7b3b45b6cce36
Maanas Nair <bee.birdiecorporation@gmail.com>
2021-09-19 17:16:55 +0530

    add sample comments

--- api/github/webhooks/index.js
+++ api/github/webhooks/index.js
@@ -1,7 +1,7 @@
 const { createNodeMiddleware, createProbot } = require("probot");
 
 const app = require("../../../index");
-
+//test
 module.exports = createNodeMiddleware(app, {
   probot: createProbot(),
   webhooksPath: "/api/github/webhooks",
