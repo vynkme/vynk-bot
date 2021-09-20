2edf396e38f352be43ca93fc119ce7d742551d56
Maanas Nair <bee.birdiecorporation@gmail.com>
2021-09-20 17:59:59 +0530

    add github workflow:`tests`, major update

--- index.js
+++ index.js
@@ -3,40 +3,12 @@
  * @param {import('probot').Probot} app
  */
 module.exports = (app) => {
-  app.log.info("the app was loaded!");
-  app.on(
-    ["pull_request.opened", "pull_request.synchronize"],
-    async (context) => {
-      app.log.info(context.payload);
+  app.on('issues.opened', async context => { 
+    const { body } = context.payload.issue;
 
-      const res = await context.octokit.repos.createDeployment(
-        context.repo({
-          ref: context.payload.pull_request.head.ref,
-          task: "deploy",
-          auto_merge: true,
-          required_contexts: [],
-          payload: {
-            schema: "rocks!",
-          }, 
-          environment: "production",
-          description: "deploying for the first time!",
-          transient_environment: false,
-          production_environment: true,
-        })
-      );
-
-      const deploymentId = res.data.id;
-      await context.octokit.repos.createDeploymentStatus(
-        context.repo({
-          deployment_id: deploymentId,
-          state: "success",
-          log_url: "https://example.com",
-          description: "Successfully set a deployment status",
-          environment_url: "https://example.com",
-          auto_inactive: true, 
-        })
-      );
-    }
-  );
-
+    const comment = context.issue({
+      body: body.includes("Thanks for the PR!"),
+    });
+    return context.github.issues.createComment(comment);
+  })
 };
